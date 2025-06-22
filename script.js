157af8787c5b124413a165ae344654dcb3f42c9d
dhruvxop19 <88497117+dhruvxop19@users.noreply.github.com>
2025-06-22 07:45:11 +0530

    Delete script.js

--- script.js
+++
@@ -1,3 +0,0 @@
-let name = "John";     // Variable that can be reassigned
-const age = 30;        // Constant variable that cannot be reassigned
-var isStudent = true;  // Older way to declare variables, function-scoped
